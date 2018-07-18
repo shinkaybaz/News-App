d14f7abcb25d0590ae406359cc296595bf539ab6
Shinkay <shinkayb@gmail.com>
2018-07-18 16:20:52 +0430

    more

---
+++ public/assets/javascript/saved.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    var articleContainer = $(".article-container");
+
+    $(document).on("click", ".btn.delete", handleArticleDelete);
+    $(document).on("click", ".btn.notes", handleArticleNotes);
+    $(document).on("click", ".btn.save", handleNoteSave);
+    $(document).on("click", ".btn.note-delete", handleNoteDelete);
+
+    initPage();
+
+    function initPage() {
+        articleContainer.empty();
+        $.get("/api/headlines?saved=true").then(function(data){
+            if (data && data.length) {
+                renderArticles(data);
+            } else {
+                renderEmpty();
+            }
+        });
+    }
+
+    function renderArticles(articles) {
+        var articlePanels = [];
+        for (var i = 0; i < articles.length; i++) {
+            articlePanels.push(createPanel(articles[i]));
+        }
+    }
+
+})
